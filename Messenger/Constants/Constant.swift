081423554cde15e41fa7f3c8ea83144dedab7261
swiftcode <mpc@junct.com>
2017-10-15 13:33:20 -0500

    Create initial UI with view controller. Create custom color file Set initial screen appearance

---
+++ Messenger/Constants/Constant.swift
@@ -0,0 +1,13 @@
+//
+//  Constant.swift
+//  Messenger
+//
+//  Created by mpc on 10/15/17.
+//  Copyright © 2017 mpc. All rights reserved.
+//
+
+import UIKit
+
+struct AppColor {
+    static let green = UIColor(red: 0/255, green: 157/255, blue: 159/255, alpha: 1)
+}
---
+++ Messenger/Controllers/MessengerViewController.swift
@@ -0,0 +1,31 @@
+//
+//  MessengerViewController.swift
+//  Messenger
+//
+//  Created by mpc on 10/15/17.
+//  Copyright © 2017 mpc. All rights reserved.
+//
+
+import UIKit
+
+class MessengerViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+
+    }
+
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
